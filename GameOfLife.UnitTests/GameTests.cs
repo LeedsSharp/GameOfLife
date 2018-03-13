b76a29ad118c1ecc878ab8a19c2aedee1976153e
autonomatt <matt@tronik.co.uk>
2018-03-13 15:08:06 +0000

    Added description and rules as comments in test classes.

--- GameOfLife.UnitTests/GameTests.cs
+++ GameOfLife.UnitTests/GameTests.cs
@@ -5,6 +5,13 @@ namespace GameOfLife.UnitTests
 {
     public class GameTests
     {
+        /* The initial pattern constitutes the seed of the system.
+         * The first generation is created by applying the above rules simultaneously to every cell in the seed—births and deaths occur simultaneously, 
+         * and the discrete moment at which this happens is sometimes called a tick.
+         * In other words, each generation is a pure function of the preceding one (F#?).
+         * The rules continue to be applied repeatedly to create further generations.
+         */
+
         [Fact]
         public void Test1()
         {
--- GameOfLife.UnitTests/GridTests.cs
+++ GameOfLife.UnitTests/GridTests.cs
@@ -5,6 +5,10 @@ namespace GameOfLife.UnitTests
 {
     public class GridTests
     {
+        /* The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells.
+         * 
+         */
+
         [Fact]
         public void Test1()
         {
